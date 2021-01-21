62037715a7c0db308579bfc63f4fe178cce501ad
mbrabenec <brabzzz@gmail.com>
2021-01-21 01:06:36 +0100

    ul

--- background/script.js
+++ background/script.js
@@ -4,8 +4,9 @@ document.querySelector("#change__button").addEventListener("click", changeCol);
 
 //event handlers
 
-function changeCol () {
+let history = [];
 
+function changeCol () {
     let values = [];
     let inputted = document.querySelectorAll("input");
     
@@ -35,12 +36,34 @@ function changeCol () {
 
     let newSelection = `rgba(${values[0]}, ${values[1]}, ${values[2]}, ${values[3]})`;
 
+    //ud background
+
     document.querySelector(".wrapper").style.background = newSelection;
     document.querySelector("span").textContent = " "+ newSelection;
 
+    // add to history (array + visual in DOM)
 
+    history.push(values);
+
+    let newRecord = document.createElement("div");
+
+    newRecord.style.height = "5vw";
+    newRecord.style.background = newSelection;
     
+    newRecord.classList.add((history.length)-1);
+    newRecord.addEventListener("click", showAgain)
 
+    let where = document.querySelector(".history__container");
+    where.insertBefore(newRecord, where.childNodes[0]);   
+}
 
+function showAgain (e) {
+    let colorInfo = history[e.target.className];
 
+    document.querySelector("#r").value = colorInfo[0];
+    document.querySelector("#g").value = colorInfo[1];
+    document.querySelector("#b").value = colorInfo[2];
+    document.querySelector("#a").value = colorInfo[3];
+
+    document.getElementById("change__button").click();
 }
